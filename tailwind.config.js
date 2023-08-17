e2a6bd2ee70cc17d2087314b1b9fb70db06073eb
Bilal Ahmed <bilalahmedim@gmail.com>
2023-08-17 20:16:15 +0530

    content added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./src/*.html"],
   theme: {
     extend: {},
   },
@@ -10,35 +10,7 @@ module.exports = {
   ],
   daisyui: {
     themes: [
-      // "light",
-      "dark",
-      // "cupcake",
-      // "bumblebee",
-      // "emerald",
-      // "corporate",
-      // "synthwave",
-      // "retro",
-      // "cyberpunk",
-      // "valentine",
-      // "halloween",
-      // "garden",
-      // "forest",
-      // "aqua",
-      // "lofi",
-      // "pastel",
-      // "fantasy",
-      // "wireframe",
-      // "black",
-      // "luxury",
-      // "dracula",
-      // "cmyk",
-      "autumn",
-      // "business",
-      // "acid",
-      // "lemonade",
-      // "night",
-      // "coffee",
-      // "winter",
+      "autumn"
     ],
   }
 }
